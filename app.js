b1c8a2ba4653eb5bcf9d108a5d1d74f990b3707f
Константин <Kostya.Okhlynin@gmail.com>
2021-02-19 17:52:46 +0300

    Added Cors

--- app.js
+++ app.js
@@ -24,6 +24,13 @@ app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, authorization');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+
+  next();
+});
 app.use(requestLogger);
 app.use(limiter);
 app.get('/crash-test', () => {
